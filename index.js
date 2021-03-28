a8a3e9df228108936b0032ed051becbaede5cace
Santiago Rincon <santiagorincon2001@gmail.com>
2021-03-28 04:05:38 -0500

    Playing with big numbers

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function diffieHelman(privA, privB){
-    const publicNumberG = 5;
-    const publicNumberP = 23;
+    const publicNumberG = 7n;
+    //89 mersenne
+    const publicNumberP = (2n**89n)-1n;
+    if(privA > publicNumberP || privB > publicNumberP){
+        throw Error("Private key cannot be bigger than P number")
+    }
 
     function getPublicKey(priv, p, g){
         return (g**priv)%p
@@ -10,10 +14,9 @@ function diffieHelman(privA, privB){
     const publicKeyA =  getPublicKey(privA, publicNumberP, publicNumberG)
     const publicKeyB =  getPublicKey(privB, publicNumberP, publicNumberG)
 
-
     const secretA = (publicKeyB**privA)%publicNumberP
     const secretB = (publicKeyA**privB)%publicNumberP
+    
     console.log(secretB === secretA ? `The secret key to use is: ${secretA}`: `Error`)
 }
-diffieHelman(6,21)
-
+diffieHelman(2474n,1579n)
